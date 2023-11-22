eb3a7c3aa37424f7517b546f19d48bd4862c96ce
weversonmp <weversonmnzs@gmail.com>
2023-11-21 22:56:07 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const { Person } = require('./person');
-// require('./modules/path');
-require('./modules/fs');
+// const { Person } = require("./person");
+const dotenv = require("dotenv");
 
-const person = new Person('Weverson');
+const connectToDatabase = require("./src/database/connect");
+
+dotenv.config();
+
+connectToDatabase();
+
+// // require("./modules/path");
+// // require("./modules/fs");
+// // require("./modules/http");
+
+require("./modules/express");
+
+// const person = new Person("Weverson");
